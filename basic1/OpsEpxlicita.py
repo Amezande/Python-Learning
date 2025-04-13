6618d77a77da7fc46d30ac22d07e72685272458a
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-04-12 18:41:02 -0600

    Create OpsEpxlicita.py

---
+++ basic1/OpsEpxlicita.py
@@ -0,0 +1,6 @@
+a= int(input("introduce un numero: "))
+b= int(input("Introduce otro numero: "))
+
+#Concatenar cadenas "+"
+#Concatenar variables ","
+print("La div. de" , a , "/" , b , "=" + str(int(a) // int(b)) + ", Con un resto de: " + str(int(a) % int(b)))
