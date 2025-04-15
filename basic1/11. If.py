cf55afcc8b150401dd0202f89544e6c580ba2876
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-04-15 14:05:08 -0600

    menus

---
+++ basic1/11. If.py
@@ -0,0 +1,8 @@
+a=int(input("Ingrese su edad: "))
+if a<=4: 
+    print("Entrada Gratuita")
+elif a>5 and a<=18:
+    print("Costo del boleto: $5")
+elif a>=19:
+    print("Costo del boleto: $10")
+    
