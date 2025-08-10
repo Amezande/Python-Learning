8074bd1b5d831d2551909242938b126ffde98283
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-08-10 17:08:06 -0600

    Create MenuInput.py

---
+++ CursoCySec/MenuInput.py
@@ -0,0 +1,20 @@
+nombre = "Santiago"
+nivel = "Avanzado"
+edad_astronauta = 24
+destino = "Venus"
+combustible = 79
+velocidad = 400
+
+print ("Nombre del alumno: ", input("Introsuce tu nombre: "))
+print (f"Nivel de conocimiento sobre python: {input('Introduce tu nivel de conocimiento: ')}\n")
+
+print ("Hola Oscar, bienvenido al curso de python.")
+print ("Tu conocimiento es avanzado, comenzaremos por la clase 1.")
+
+print ("El nombre del profesor es: ", nombre)
+print ("Su nivel es: ", nivel)
+
+nombre = input ("Introduce tu nombre")
+print (nombre)
+print (f"Hola, soy",nombre,"Tengo:",edad_astronauta,"años y mi próximo destino es",destino)
+print ("Estoy navegando a",velocidad,"Km/s con",combustible,"% de combustible restante hacia",destino)
