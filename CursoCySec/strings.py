c4e5878e5c897c6167b62abff22b011056fbdb8e
Axel Amacende <63935105+Amezande@users.noreply.github.com>
2025-08-10 17:07:33 -0600

    Create strings.py

---
+++ CursoCySec/strings.py
@@ -0,0 +1,15 @@
+print ("Hoy es Sabado")
+print ("Hoy \nes \nSabado \n")
+print ("""Hoy
+es
+Sabado""")
+print (("Hoy"
+        "es"
+        "Sabado"
+        ))
+
+# F strings
+
+print (f"Cadena de texto combinada con un numero {10}") #1
+print ("Cadena de texto combinada con un numero", 10)   #2
+print (f"El numero 10.5 tiene el siguiente tipo: {type(10.5)}")
